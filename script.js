57f69b28f04a695b83761e707716615b6bf3e02d
Edson Pereira <ed.1206@gmail.com>
2022-06-07 18:18:53 -0300

    iniciado requisito 3

---
+++ script.js
@@ -0,0 +1,28 @@
+function emailValidaction() {
+  const email = document.querySelector('#email');
+  if (email.value === 'tryber@teste.com') {
+    return true;
+  }
+  return false;
+}
+
+function passwordValidation() {
+  const password = document.querySelector('#password');
+  if (password.value === '123456') {
+    return true;
+  }
+  return false;
+}
+
+function submit() {
+  if (emailValidaction === true && passwordValidation === true) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos');
+  }
+}
+
+// window.onload = function () {
+//   const submitBtn = document.querySelector('submit-button');
+//   submitBtn.addEventListener('click', submit);
+// };
