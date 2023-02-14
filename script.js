4736e403aa0c7efd744569634b945b7a4b8cb0fc
Edson Pereira <ed.1206@gmail.com>
2023-02-13 21:40:15 -0300

    refactor: :recycle: Improve form data e refactor the code

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+const main = document.getElementById('main');
+const form = document.querySelector('#evaluation-form');
+const formButton = document.querySelector('#submit-btn');
+const agreement = document.querySelector('#agreement');
+const email = document.getElementById('email');
+const senha = document.getElementById('password');
+const describe = document.querySelector('#textarea');
+const count = document.querySelector('#counter');
 const submitButton = document.querySelector('.submit-button');
+const element = document.createElement('form');
+const trybewartsFormLogo = document.getElementById('trybewarts-forms-logo');
+
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
-  const email = document.getElementById('email');
-  const senha = document.getElementById('password');
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
@@ -10,19 +19,15 @@ submitButton.addEventListener('click', (event) => {
   }
 });
 
-const formButton = document.querySelector('#submit-btn');
-const agreement = document.querySelector('#agreement');
 agreement.addEventListener('change', (event) => {
   event.preventDefault();
   formButton.disabled = !agreement.checked;
 });
 
-const desc = document.querySelector('#textarea');
-desc.addEventListener('input', (event) => {
+describe.addEventListener('input', (event) => {
   const maxText = 500;
-  const textArea = desc.value.length;
+  const textArea = describe.value.length;
   const rest = maxText - textArea;
-  const count = document.querySelector('#counter');
   count.innerText = `Caracteres restantes: ${rest}`;
   if (textArea >= maxText) {
     event.preventDefault();
@@ -33,3 +38,31 @@ desc.addEventListener('input', (event) => {
     count.setAttribute('style', 'color: black');
   }
 });
+
+function getChecked() {
+  return [...document.querySelectorAll('input[class="subject"]:checked')]
+    .map((input) => input.value)
+    .join(', ');
+}
+
+function showAfterSubmit(event) {
+  event.preventDefault();
+  const selected = getChecked();
+  const name = document.querySelector('#input-name').value;
+  const lastName = document.querySelector('#input-lastname').value;
+  const house = document.querySelector('#house').value;
+  const mail = document.querySelector('#input-email').value;
+  const family = document.querySelector('input[name ="family"]:checked').value;
+  const rank = document.querySelector('input[name ="rate"]:checked').value;
+  const text = describe.value;
+  form.style.display = 'none';
+  element.id = 'form-data';
+  main.appendChild(element);
+  main.insertBefore(element, trybewartsFormLogo);
+  element.innerHTML = `<p>Nome: ${name} ${lastName}</p><p>Email: ${mail}</p>
+        <p>Casa: ${house}</p><p>Família: ${family}</p>
+        <p>Matérias: ${selected}</p><p>Avaliação: ${rank}</p>
+        <p>Observações: ${text}</p>`;
+}
+
+formButton.addEventListener('click', showAfterSubmit);
